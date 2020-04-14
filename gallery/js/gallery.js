1b6b8fbbfdb0a4dffb83373302829e646a099521
Sapna Rai <sapnarai196@gmail.com>
2020-04-14 18:33:29 +0530

    control also given to arrow keys using keydown events

--- gallery/js/gallery.js
+++ gallery/js/gallery.js
@@ -1,17 +1,17 @@
 function Gallery(images, container) {
-        this.images = images;
-        this.container = container;
-        this.initializeGallery();
-        this.startListeningOnThumbnails();
+    this.images = images;
+    this.container = container;
+    this.initializeGallery();
+    this.startListeningOnThumbnails();
 }
 
-Gallery.prototype.initializeGallery = function() {
+Gallery.prototype.initializeGallery = function () {
     this.createDomPlaceholders();
     // this.mainImage.src = this.images[0];
     this.loadThumbnails();
 };
 
-Gallery.prototype.createDomPlaceholders = function() {
+Gallery.prototype.createDomPlaceholders = function () {
     //Create a large pic dom node inside container
     //Create a div with thumbnails inside
     let mainImageTemplate = document.querySelector('#overlay-container');
@@ -23,9 +23,9 @@ Gallery.prototype.createDomPlaceholders = function() {
     this.leftarrowButton = mainImageDocFragment.querySelector('.full-overlay .arrow-left');
     this.rightarrowButton = mainImageDocFragment.querySelector('.full-overlay .arrow-right');
     document.body.appendChild(mainImageDocFragment);
-    
-    
-    
+
+
+
 
     this.thumbnailsContainer = document.createElement('div');
     this.thumbnailsContainer.classList.add("thumbnails-container");
@@ -34,8 +34,8 @@ Gallery.prototype.createDomPlaceholders = function() {
 };
 
 
-Gallery.prototype.loadThumbnails = function(){
-    for(let index in this.images){
+Gallery.prototype.loadThumbnails = function () {
+    for (let index in this.images) {
         var image = this.images[index];
         var imageDom = document.createElement('img');
         imageDom.src = image;
@@ -44,28 +44,28 @@ Gallery.prototype.loadThumbnails = function(){
     }
 };
 
-Gallery.prototype.startListeningOnThumbnails = function() {
+Gallery.prototype.startListeningOnThumbnails = function () {
     // save the reference of this so that this will not  change its definition
     var self = this;
-    this.thumbnailsContainer.addEventListener('click', function(event){
-        if(event.target instanceof HTMLImageElement) {
+    this.thumbnailsContainer.addEventListener('click', function (event) {
+        if (event.target instanceof HTMLImageElement) {
             self.mainImage.src = event.target.src;
             self.mainImgContainer.style.display = "block";
             self.mainImage.dataset.index = event.target.dataset.index;
-            
+
         }
     })
-    this.closeButton.addEventListener('click', function(){
+    this.closeButton.addEventListener('click', function () {
         self.mainImgContainer.style.display = "none";
     })
-    this.leftarrowButton.addEventListener('click', function(event){
+    this.leftarrowButton.addEventListener('click', function (event) {
         let index = parseInt(self.mainImage.dataset.index);
         let previousIndex = index == 0 ? self.images.length - 1 : index - 1;
         let previousUrl = self.images[previousIndex];
         self.mainImage.src = previousUrl;
-        self.mainImage.dataset.index = previousIndex; 
-    } )
-    this.rightarrowButton.addEventListener('click', function(event){
+        self.mainImage.dataset.index = previousIndex;
+    })
+    this.rightarrowButton.addEventListener('click', function (event) {
         let index = parseInt(self.mainImage.dataset.index);
         let nextindex = index == self.images.length - 1 ? 0 : index + 1;
         let nextUrl = self.images[nextindex];
@@ -73,5 +73,24 @@ Gallery.prototype.startListeningOnThumbnails = function() {
         self.mainImage.dataset.index = nextindex;
 
     })
-    
+    document.addEventListener('keydown', function (event) {
+        if (event.keyCode == 27) {
+            self.mainImgContainer.style.display = "none";
+        }
+        else if (event.keyCode == 37) {
+            let index = parseInt(self.mainImage.dataset.index);
+            let previousIndex = index == 0 ? self.images.length - 1 : index - 1;
+            let previousUrl = self.images[previousIndex];
+            self.mainImage.src = previousUrl;
+            self.mainImage.dataset.index = previousIndex;
+        } else if (event.keyCode == 39) {
+            let index = parseInt(self.mainImage.dataset.index);
+            let nextindex = index == self.images.length - 1 ? 0 : index + 1;
+            let nextUrl = self.images[nextindex];
+            self.mainImage.src = nextUrl;
+            self.mainImage.dataset.index = nextindex;
+        }
+    })
+
+
 };
